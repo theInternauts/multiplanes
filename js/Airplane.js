a86445fbb6c9389d416bf2fb3a3d1b6b23b47ad5
Christopher "Cricket" Wallace <cricketw@wearetheinternauts.com>
2014-01-01 23:56:03 -0800

    adding object to Airplane class to store the movement state of each instance

--- js/Airplane.js
+++ js/Airplane.js
@@ -1,5 +1,11 @@
 function Airplane( color ){
+	var geometry = new THREE.CylinderGeometry( 0, 10, 100, 3 )
+	var material = new THREE.MeshBasicMaterial()
 	this.color = (color) ? color : '0x0000FF'
+	this.mesh = new THREE.Mesh(geometry, material)
+	this.moveState = { up: false, down: false, left: false, right: false, forward: false, back: false, pitchUp: false, pitchDown: false, yawLeft: false, yawRight: false, rollLeft: false, rollRight: false }
+
+	//return airplane
 }
 
 Airplane.prototype.getColor = function(){ return this.color }
